4b5940a611d1b691272ef85432d92fe90ea03adf
MihirKotecha <mihir.k1@ahduni.edu.in>
2023-01-14 17:55:56 +0530

    900

---
+++ GamewithSticks.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main(){
+    int a,b;
+    cin>>a>>b;
+
+        int k = min(a,b);
+        if(k%2==0){
+            cout<<"Malvika"<<endl;
+        }
+        else{
+            cout<<"Akshat"<<endl;
+        }
+
+
+    return 0;
+}
