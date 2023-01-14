e3117cc5e7d83fc73a44d891fc921afc1028d604
MihirKotecha <mihir.k1@ahduni.edu.in>
2023-01-14 17:45:43 +0530

    800

---
+++ LearnfromMath.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cin>>x;
+
+    if(x%2!=0){
+         cout<<9<<" "<<x-9<<endl;
+    }
+    else{
+       
+        cout<<4<<" "<<x-4<<endl;
+    }
+
+    return 0;
+}
